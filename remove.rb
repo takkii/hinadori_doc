14c6291e4918a93004ccea705c51c0e516e153b6
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-07-27 12:44:16 +0900

    Update.

---
+++ remove.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+#!/usr/bin/ruby
+
+require 'fileutils'
+
+# Installer runner.
+class UnInstallerRunner
+  # default encoding utf-8, change encode here.
+  def self.encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
+  end
+
+  def self.run
+    encoding_style
+    FileUtils.rm_rf(File.expand_path('~/hinadori_doc'))
+  end
+end
+
+UnInstallerRunner.run
